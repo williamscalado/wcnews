44fc2cb344077f886cb020d624f8970cd24b71eb
Williams Calado <williamscalado@gmail.com>
2022-05-30 15:08:57 -0300

    The first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>WC News</title>
+    </Head>
+    <h1>Hello Word!</h1>
+    </>
+    
+  )
+}
+
+export default Home
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+    return(
+        <>
+        <Head>
+            <title>WC news | Produtos</title>
+        </Head>
+        <h1>Products</h1>
+        </>
+    )
+}
+
+
+export default Products;
