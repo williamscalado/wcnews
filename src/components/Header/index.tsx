e446d09ce8f23ee01b5c478c73ca5295fd7a227f
Williams Calado <williamscalado@gmail.com>
2022-06-01 14:51:10 -0300

    create button Sing in and Menu list to mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,46 @@
+import Image from 'next/image'
+import { FaBars } from 'react-icons/fa'
 import { ButtonSignIn } from '../ButtonSingIn'
 import style from './style.module.scss'
 
 export const Header = () => {
+
+    const menuList = [
+        {
+            name: 'Home',
+            link: '#',
+            active: true
+        },
+        {
+            name: 'News',
+            link: '#',            
+            active: false
+        },
+        {
+            name: 'Contact',
+            link: '#',            
+            active: false
+        },
+    ]
+
 return(
     <header className={style.headerContainer}>
         <div className={style.headerContent}>
-            <img src="/images/logo.png" alt="Wc News" />
-            <nav>
-                <a href="" className={style.isActive}>Pagina Inicla</a>
-                <a href="" >Notícias</a>
-                <a href="">Contato</a>
+             <nav className={style.menuMobile}>
+                <FaBars width={25} height={25} />
             </nav>
+            <Image src="/images/logo.png" alt="Wc News" width={150} height={40} quality={100} className={style.Logo} />
+            <nav>
+                <ul>{
+                    menuList.map((data)=>{                    
+                    return(
+                    <li key={data.name}><a href={data.link} className={data.active ? style.isActive : ""}>{data.name}</a></li>                   
+                        );
+                    })
+                }
+                </ul> 
+            </nav>
+           
             <ButtonSignIn />
         </div>
         
